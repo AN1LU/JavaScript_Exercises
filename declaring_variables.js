fccd393505791e2e2fb79a674a26a4f75bda38e2
Lucia Pelayo <143049839+AN1LU@users.noreply.github.com>
2025-06-01 06:53:04 -0600

    Add files via upload

---
+++ declaring_variables.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Ana Lucia' // first name of a person
+let lastName = 'Pelayo' // last name of a person
+let country = 'Mexico' // country
+let city = 'Guadalajara' // capital city
+let age = 22 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Ana Lucia', //name of a person
+  job = 'teacher',
+  live = 'Mexico'
