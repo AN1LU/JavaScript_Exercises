882395f8ac201c767d3bec5cf5eb26e18e7354a3
AN1LU <analuciapelayom@gmail.com>
2025-06-28 19:32:09 -0600

    Web storages practice, lines of the script and the results

---
+++ web_storages.js
@@ -0,0 +1,10 @@
+//Exercises to master JavaScript
+//DAY 17/30
+
+localStorage.setItem('firstName', 'Ana Lucia') // since the value is string we do not stringify it
+localStorage.setItem('lastName', 'Pelayo') // since the value is string we do not stringify it
+
+//age country city
+
+
+console.log(localStorage)
