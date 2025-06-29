03be485afc1fe10fdae3f7cddedf6ef594240b7f
AN1LU <analuciapelayom@gmail.com>
2025-06-28 19:38:38 -0600

    error handling exercise

---
+++ error_handling.js
@@ -0,0 +1,19 @@
+//Exercises to master JavaScript
+//DAY 14/30
+
+// Structure
+
+// try {
+//   // code that may throw an error
+// } catch (err) {
+//   // code to be executed if an error occurs
+// } finally {
+//   // code to be executed regardless of an error occurs or not
+// }
+
+try {
+  let lastName = 'Pelayo'
+  let fullName = fistName + ' ' + lastName
+} catch (err) {
+  console.log(err)
+}
