07ec1a0fc380a4c32e9952a5bf6d375c8a9b6f62
AN1LU <analuciapelayom@gmail.com>
2025-06-28 20:19:08 -0600

    added the promises part in this file because for this exercise I will need to access this API in order to get the data for the visualization

---
+++ countries_visualization.js
@@ -0,0 +1,13 @@
+
+const url = 'https://api.countrylayer.com/v2/all' // countries api
+const access_key = 'f45309ae7b821c05da55d8d09c852011'
+fetch(url + '?access_key=' + access_key) // fetching the API data
+  .then(response => response.json()) // accessing the API data as JSON
+  .then(data => {
+    data.forEach(country => {
+      console.log(`Country: ${country.name}, Capital: ${country.capital}, Population: ${country.population}, Area: ${country.area} km²`)
+    })
+  })
+  .catch(error => console.error(error)) // handling error if something wrong happens
+
+  
